a5896863fc397e4e32c90f11b8b86f74fb059135
hockey4life63 <Amelancon68@gmail.com>
2017-04-23 23:36:34 -0700

    made functions to do setup and allow game to be replayed and started to add hangman animation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,117 @@
-//word list
-var words = [ "fallout", "mass effect","tron", "donkey kong","golden eye"]
-//chose word at random from list
-var compWord = words[Math.floor(Math.random()*words.length)]
-console.log(compWord)//for testing
-//creates the blank letter spaces and assigns a id based on index
-for (var i = 0; i < compWord.length; i++) {
-	var item = document.createElement("li");
-	item.className = 'guessWordLetter';
-	item.id = compWord[i] === " " ? "space" : i;
-	item.innerHTML = item.id === "space" ? " " : "_";
-	document.getElementById("guessWord").appendChild(item);
-
-}//for
+var hangManObj = new Object();
 //string of all guessed letters
-var guessedLetters = "";
+hangManObj.guessedLetters = "";
+hangManObj.maxGuess =9;
+hangManObj.totalGuess =0;
+hangManObj.totalMisses =0;
+hangManObj.alphabet = "abcdefghijklmnopqrstuvwxyz"
 // bool for if won or lost
-var won;
+hangManObj.won;
+//word list
+hangManObj.words = [ "fallout", "mass effect","tron", "donkey kong","golden eye"];
+hangManObj.compWord
+hangManObj.newWord = function(){
+	hangManObj.compWord = hangManObj.words[Math.floor(Math.random()*hangManObj.words.length)]
+}
+function gameSetup(){
+	document.getElementById("gameArea").innerHTML ='<form id="guessForm"> \
+						<input type="text" name="guess" id="userGuess" maxlength="1"> \
+						<button type="button" id="guessButton">Guess</button> \
+						<br>\
+						<input type="text" name="solve" id="userSolve">\
+						<button type="button" id="solveButton">Solve</button>\
+						<br>\
+					</form>\
+					<ul id="guessWord"></ul>\
+					<h2 class="infoTitle">Guessed Letters</h2>\
+					<div id="guessedList"></div>\
+					<div id="gameLog"></div>'
+}//setups the play area
+function newWordSpace(word){
+	//creates the blank letter spaces and assigns a id based on index
+	document.getElementById("guessWord").innerHTML ="";
+	for (var i = 0; i < word.length; i++) {
+		var item = document.createElement("li");
+		item.className = 'guessWordLetter';
+		//checks if space and assigns id space otherwise gives id of index positon
+		item.id = word[i] === " " ? "space" : i;
+		//if id of space puts space otherwise puts _
+		item.innerHTML = item.id === "space" ? " " : "_";
+		document.getElementById("guessWord").appendChild(item);
+	}
+}
+function newGame(){
+	hangManObj.newWord();
+	newWordSpace(hangManObj.compWord);
+	hangManObj.totalGuess = 0;
+	hangManObj.totalMisses = 0;
+	hangManObj.guessedLetters = "";
+	document.getElementById("guessedList").innerHTML = "";
+}
+//chose word at random from list
+console.log(hangManObj.compWord)//for testing
+
+//to display to individual log bout formhangManObj
+
+/*hangManObj
+  words
+  compWord
+  guessedLetters
+  maxGuess
+  totalGuess
+  totalMisses
+  asplabet
+  won
+  func newWord*/
+var permLog = document.getElementById("gameLog");
+var log = document.getElementById("lastLog");
 //on guess button click runs mainGame
-document.getElementById('guessButton').addEventListener("click",function () {mainGame();})
+document.getElementById("startButton").addEventListener("click", function(){
+	gameSetup();
+	newGame();
+	document.getElementById('guessButton').addEventListener("click",function () {mainGame();})
+	document.getElementById("solveButton").addEventListener("click" ,function() {solve();})
+	document.getElementById("startButton").setAttribute("class", "hidden");
+});
 
 function mainGame(){
 	//grabs the letter guesses
 	var guessLetter = document.getElementById("userGuess").value;
+	//Check if a vaild char
+	if(hangManObj.alphabet.indexOf(guessLetter)=== -1){
+		log.innerHTML ="Invaild option";
+		permLog.innerHTML += "Invaild option <br>"
+		guessLetter.reset;
+		document.getElementById("guessForm").reset();
+		return
+	}
 	//check against gessed letter and return if already guessed
-	for (var i = 0; i < guessedLetters.length; i++) {
-		if(guessedLetters[i] === guessLetter){
-			console.log("already guessed that try agian")
+	for (var i = 0; i < hangManObj.guessedLetters.length; i++) {
+		if(hangManObj.guessedLetters[i] === guessLetter){
+			log.innerHTML = "You already guessed "+ guessLetter + " that try agian";
+			permLog.innerHTML += "You already guessed "+ guessLetter + " that try agian <br>";
+			guessLetter.reset;
+			document.getElementById("guessForm").reset();
 			return
 		}
 	}
 	//loop thru word and check if letter appears in it
-	for (var i = 0; i<compWord.length; i++) {
+	for (var i = 0; i<hangManObj.compWord.length; i++) {
 		//grabs letter based on index which is also its id
 		var letter = document.getElementById(i);
 		//reveal letter if appears
-		if(guessLetter === compWord[i]){
-			letter.innerHTML = compWord[i]
+		if(guessLetter === hangManObj.compWord[i]){
+			letter.innerHTML = hangManObj.compWord[i]
 
 		}//if
 	}//for
 	//adds letter to already guessed letter list
-	guessedLetters += guessLetter;
+	hangManObj.guessedLetters += guessLetter;
+	document.getElementById("guessedList").innerHTML = "";
+
+	for (var i = 0; i < hangManObj.guessedLetters.length; i++) {
+		document.getElementById("guessedList").innerHTML += hangManObj.guessedLetters[i];
+	}
 	guessLetter.reset;
 	//clears form
 	document.getElementById("guessForm").reset();
@@ -48,20 +119,39 @@ function mainGame(){
 	var check = document.getElementsByClassName("guessWordLetter");
 	//loops thru elements 
 	for (var i = 0; i < check.length; i++) {
-		console.log("checking")
 		if(check[i].innerHTML === "_"){
 			return
 		}
 	}
-	won = true;
+	hangManObj.won = true;
 	gameEnd(won);
+
+}//mainGAme
+
+function solve(){
+	var userSolveWord = document.getElementById("userSolve").value;
+	if(userSolveWord === hangManObj.compWord){
+		hangManObj.won=true;
+		gameEnd(hangManObj.won);
+	}
+	else{
+		hangManObj.won = false;
+		gameEnd(hangManObj.won);
+	}
 
 }
 
 function gameEnd(won){
+	//disables both form inputs
+	document.getElementById("guessForm").reset();
+	document.getElementById("userGuess").disabled = true;
+	document.getElementById("userSolve").disabled = true;
+	document.getElementById("startButton").classList.remove("hidden")
 	if(won){
-		document.getElementById("userGuess").disabled = true;
-		document.getElementById("userSolve").disabled = true;
+		log.innerHTML = "You Won!"
+	}
+	else{
+		log.innerHTML = "Sorry you lost :/ the word was " + hangManObj.compWord;
 	}
 }
 //code example
